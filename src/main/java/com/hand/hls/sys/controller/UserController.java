26f5133fd56d83671e4959fcf44881ee8d4df8ff
songyongjian <songyongjian@outlook.com>
2018-08-03 09:24:30 +0800

    添加 spring mvc 登录

--- src/main/java/com/hand/hls/sys/controller/UserController.java
+++ src/main/java/com/hand/hls/sys/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.hand.hls.sys.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import com.hand.hls.sys.dto.User;
 import com.hand.hls.sys.service.IUserService;
@@ -30,4 +31,39 @@ public class UserController {
         request.setAttribute("error", "没有找到该用户！");
         return "error";
     }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String toLogin() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(User user, Model model, HttpSession session) {
+        //获取用户名和密码
+        String username = user.getUserName();
+        String password = user.getPassword();
+        //些处横板从数据库中获取对用户名和密码后进行判断
+        if (username != null && username.equals("admin") && password != null && password.equals("admin")) {
+            //将用户对象添加到Session中
+            session.setAttribute("USER_SESSION", user);
+            //重定向到主页面的跳转方法
+            return "redirect:main";
+        }
+        model.addAttribute("msg", "用户名或密码错误，请重新登录！");
+        return "login";
+    }
+
+    @RequestMapping(value = "/main")
+    public String toMain() {
+        return "main";
+    }
+
+    @RequestMapping(value = "/logout")
+    public String logout(HttpSession session) {
+        //清除session
+        session.invalidate();
+        //重定向到登录页面的跳转方法
+        return "redirect:login";
+    }
+
 }
